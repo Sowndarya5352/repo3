17aab4ae318b954c6efd3c181d64bd202957fc29
Sowndarya5352 <sowndaryas.ca21@kascsathy.ac.in>
2023-07-31 09:39:27 +0000

    first commit

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+        int i;
+        int sum=6;
+        for(i=1; i<=5; i=i+1) {
+            sum = sum + i;         
+        }
+        printf("%d \n",sum,i);
+        return 0;
+}
